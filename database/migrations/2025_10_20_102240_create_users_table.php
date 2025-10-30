82b392ce50e073b095c3cbfc5b23b81dce9f0161
Dani Snow <fullsdeveloper981@outlook.com>
2025-10-30 10:30:52 +0100

    UserSoftDelete

--- database/migrations/2025_10_20_102240_create_users_table.php
+++ database/migrations/2025_10_20_102240_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('login_code')->nullable();
             $table->timestamp('login_code_expires_at')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
