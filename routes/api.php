862aa8467d6c7bd31f99f6af461be3c8d0b617ce
Dani Snow <fullsdeveloper981@outlook.com>
2025-11-28 11:23:55 +0100

    minor

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\API\EmailVerificationController;
 use Illuminate\Support\Facades\Route;
 
 
-       Route::middleware('throttle:4,1')->group(function () {
+       Route::middleware('throttle:3,1')->group(function () {
          Route::post('/register', [AuthorizationController::class, 'register']);
          Route::post('/reset-password', [AuthenticateController::class, 'resetPassword']);
          Route::post('/verification-code-password-reset',[AuthenticateController::class, 'verificationCode']);
